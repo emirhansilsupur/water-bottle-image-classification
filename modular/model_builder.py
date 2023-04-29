84d8c22a6117a7079a11209bd3f889c13f5a8840
emirhansilsupur <sr.emirhn@gmail.com>
2023-04-29 15:44:45 +0300

    Modify the output layer of EfficientNet_b0

--- modular/model_builder.py
+++ modular/model_builder.py
@@ -4,15 +4,9 @@ Optionally, it freezes the feature weights and prints a summary of the model's a
 """
 import torch
 import torchvision
-from torchinfo import summary
 
 
-def create_model(
-    model_name: str,
-    num_classes: int,
-    freeze_features: bool = True,
-    summary_model: bool = True,
-):
+def create_model(model_name: str, num_classes: int, freeze_features: bool = True):
     """
     Creates and returns a PyTorch pre-trained models based on the given model name and number of classes.
 
@@ -20,7 +14,6 @@ def create_model(
     - model_name (str): The name of the model to create. It can be one of the following: "EfficientNet_B0", "ResNet18", or "GoogLeNet".
     - num_classes (int): The number of output classes.
     - freeze_features (bool, optional): If True, freezes the weights of the feature extractor layers of the model. Default is True.
-    - summary_model (bool, optional): If True, prints a summary of the model. Default is True.
 
     Returns:
     - model (torch.nn.Module): The created PyTorch model.
@@ -32,11 +25,17 @@ def create_model(
         model = torchvision.models.efficientnet_b0(weights=weights).to(device)
         # Freeze the model features if specified
         if freeze_features:
-            for param in weights.parameters():
+            for param in model.features.parameters():
                 param.requires_grad = False
         # Update the last layer of the model for the specified number of classes
-        num_ftrs = model._fc.in_features
-        model._fc = torch.nn.Linear(num_ftrs, num_classes)
+        model.classifier = torch.nn.Sequential(
+            torch.nn.Dropout(p=0.25, inplace=True),
+            torch.nn.Linear(
+                in_features=1280,
+                out_features=3,  # same number of output units as our number of classes
+                bias=True,
+            ),
+        ).to(device)
         model_transform = weights.transforms()
 
     elif model_name == "ResNet18":
@@ -46,7 +45,7 @@ def create_model(
             for param in model.parameters():
                 param.requires_grad = False
         num_ftrs = model.fc.in_features
-        model.fc = torch.nn.Linear(num_ftrs, num_classes)
+        model.fc = torch.nn.Linear(num_ftrs, num_classes).to(device)
         model_transform = weights.transforms()
 
     elif model_name == "GoogLeNet":
@@ -56,16 +55,7 @@ def create_model(
             for param in model.parameters():
                 param.requires_grad = False
         num_ftrs = model.fc.in_features
-        model.fc = torch.nn.Linear(num_ftrs, num_classes)
+        model.fc = torch.nn.Linear(num_ftrs, num_classes).to(device)
         model_transform = weights.transforms()
-    # Print a summary of the model if summary_model is True
-    if summary_model:
-        summary(
-            model=model,
-            input_size=(3, 224, 224),
-            col_names=["input_size", "output_size", "num_params", "trainable"],
-            col_width=20,
-            row_settings=["var_names"],
-        )
 
     return model, model_transform
