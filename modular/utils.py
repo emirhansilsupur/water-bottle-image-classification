c10283ce618e670a91063380415d6ec3dfc8d45e
emirhansilsupur <sr.emirhn@gmail.com>
2023-08-08 16:13:03 +0300

    Move create_writer to train.py

--- modular/utils.py
+++ modular/utils.py
@@ -3,7 +3,6 @@ File containing various utility functions for PyTorch model training.
 """
 import torch
 from pathlib import Path
-from datetime import datetime
 import os
 
 
@@ -31,40 +30,3 @@ def save_model(model: torch.nn.Module, target_dir: str, model_name: str):
 
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(), f=model_save_path)
-
-
-def create_writer(
-    experiment_name: str,
-    model_name: str,
-    epoch: int,
-    lr: float,
-) -> torch.utils.tensorboard.writer.SummaryWriter:
-    """Creates a torch.utils.tensorboard.writer.SummaryWriter() instance saving to a specific log_dir.
-
-    log_dir is a combination of runs/timestamp/experiment_name/model_name/extra.
-
-    Where timestamp is the current date in YYYY-MM-DD format.
-
-    Args:
-        experiment_name (str): Name of experiment.
-        model_name (str): Name of model.
-        epoch (int): Epoch number.
-        lr (float): Learning rate used in the experiment.
-
-    Returns:
-        torch.utils.tensorboard.writer.SummaryWriter: Instance of a writer saving to log_dir.
-
-
-    """
-    # Get timestamp of current date (all experiments on certain day live in same folder)
-    timestamp = datetime.now().strftime(
-        "%Y-%m-%d"
-    )  # returns current date in YYYY-MM-DD format
-
-    log_dir_parts = ["runs", timestamp, experiment_name, model_name, epoch, lr]
-
-    # Create log directory path
-    log_dir = os.path.join(*log_dir_parts)
-
-    print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
-    return torch.utils.tensorboard.writer.SummaryWriter(log_dir=log_dir)
